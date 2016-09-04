e9e4abfaed05414b254616e559632b1e475637c8
Pedro Côrte-Real <pedro@pedrocr.net>
2016-09-04 23:02:00 +0100

    Use byteorder as it is faster than hand-rolled
    My hand rolled code does math to create 16/32 bit values when in a
    lot of cases a simple transmute is enough. Using byteorder instead
    does this by default and is quite a bit faster as a result.

--- src/decoders/basics.rs
+++ src/decoders/basics.rs
@@ -2,6 +2,9 @@ extern crate crossbeam;
 use std;
 use decoders::NUM_CORES;
 
+extern crate byteorder;
+use self::byteorder::{BigEndian, LittleEndian, ByteOrder};
+
 #[derive(Debug, Copy, Clone)]
 pub struct Endian {
   big: bool,
@@ -29,26 +32,20 @@ impl Endian {
 pub static BIG_ENDIAN: Endian = Endian{big: true};
 pub static LITTLE_ENDIAN: Endian = Endian{big: false};
 
-#[allow(non_snake_case)] pub fn BEu32(buf: &[u8], pos: usize) -> u32 {
-  (buf[pos] as u32) << 24 |
-  (buf[pos+1] as u32) << 16 |
-  (buf[pos+2] as u32) << 8 |
-  (buf[pos+3] as u32)
+#[allow(non_snake_case)] #[inline] pub fn BEu32(buf: &[u8], pos: usize) -> u32 {
+  BigEndian::read_u32(&buf[pos..pos+4])
 }
 
-#[allow(non_snake_case)] pub fn LEu32(buf: &[u8], pos: usize) -> u32 {
-  (buf[pos] as u32) |
-  (buf[pos+1] as u32) << 8 |
-  (buf[pos+2] as u32) << 16 |
-  (buf[pos+3] as u32) << 24
+#[allow(non_snake_case)] #[inline] pub fn LEu32(buf: &[u8], pos: usize) -> u32 {
+  LittleEndian::read_u32(&buf[pos..pos+4])
 }
 
-#[allow(non_snake_case)] pub fn BEu16(buf: &[u8], pos: usize) -> u16 {
-  (buf[pos] as u16) << 8 | (buf[pos+1] as u16)
+#[allow(non_snake_case)] #[inline] pub fn BEu16(buf: &[u8], pos: usize) -> u16 {
+  BigEndian::read_u16(&buf[pos..pos+2])
 }
 
-#[allow(non_snake_case)] pub fn LEu16(buf: &[u8], pos: usize) -> u16 {
-  (buf[pos] as u16) | (buf[pos+1] as u16) << 8
+#[allow(non_snake_case)] #[inline] pub fn LEu16(buf: &[u8], pos: usize) -> u16 {
+  LittleEndian::read_u16(&buf[pos..pos+2])
 }
 
 pub fn decode_12be(buf: &[u8], width: usize, height: usize) -> Vec<u16> {
