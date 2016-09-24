58b8f5cc4e5140b6785364e5df762fb7da659746
Pedro Côrte-Real <pedro@pedrocr.net>
2016-09-24 21:19:12 +0100

    demosaic: fix several wrong bits

--- src/imageops/demosaic.rs
+++ src/imageops/demosaic.rs
@@ -9,8 +9,8 @@ pub fn ppg(img: &Image, inb: &[f32]) -> Vec<f32> {
   // First we set the colors we already have
   let mut ipos = 0;
   let mut opos = 0;
-  for row in 0..img.width {
-    for col in 0..img.height {
+  for row in 0..img.height {
+    for col in 0..img.width {
       let color = fcol(img, row, col);
       out[opos+color] = inb[ipos];
       ipos += 1;
@@ -24,17 +24,18 @@ pub fn ppg(img: &Image, inb: &[f32]) -> Vec<f32> {
     for col in 0..img.width {
       let mut sums: [f32; 4] = [0.0;4];
       let mut counts: [f32; 4] = [0.0;4];
+      let color = fcol(img, row, col);
 
-      for y in (cmp::max(0,(row as isize)-1) as usize) .. cmp::min(img.height, row+1) {
-        for x in (cmp::max(0,(col as isize)-1) as usize) .. cmp::min(img.width, col+1) {
-          let color = fcol(img, x, y);
-          //println!("Fetching color {} at pos {}x{}", color, x, y);
-          sums[color] += inb[y*img.width+x];
-          counts[color] += 1.0;
+      for y in (cmp::max(0,(row as isize)-1) as usize) .. cmp::min(img.height, row+2) {
+        for x in (cmp::max(0,(col as isize)-1) as usize) .. cmp::min(img.width, col+2) {
+          let c = fcol(img, y, x);
+          if c != color {
+            sums[c] += inb[y*img.width+x];
+            counts[c] += 1.0;
+          }
         }
       }
 
-      let color = fcol(img, row, col);
       for c in 0..3 {
         if c != color {
           out[(row*img.width+col)*3+c] = sums[c] / counts[c];
