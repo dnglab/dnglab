a43127b2c765dbb9576351fd67f5fbcdf846089b
Pedro Côrte-Real <pedro@pedrocr.net>
2016-09-25 17:29:28 +0100

    gamma: use rec709 gamma constants for PPM

--- src/imageops/gamma.rs
+++ src/imageops/gamma.rs
@@ -3,10 +3,10 @@ use decoders::Image;
 pub fn gamma(img: &Image, inb: &[f32]) -> Vec<f32> {
   let mut out: Vec<f32> = vec![0.0; (img.width*img.height*3) as usize];
 
-  let g: f32 = 1.0 / 2.4;
-  let f: f32 = 0.055;
-  let min: f32 = 0.0031308;
-  let mul: f32 = 12.92;
+  let g: f32 = 0.45;
+  let f: f32 = 0.099;
+  let min: f32 = 0.018;
+  let mul: f32 = 4.5;
   
 
   let mut pos = 0;
