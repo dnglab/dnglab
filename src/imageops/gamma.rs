0baeba83a966848a7122e1ac26c60d53ba95d0b8
Pedro Côrte-Real <pedro@pedrocr.net>
2016-10-01 21:47:05 +0100

    gamma: speedup by precomputing the powf

--- src/imageops/gamma.rs
+++ src/imageops/gamma.rs
@@ -8,13 +8,21 @@ pub fn gamma(img: &Image, inb: &[f32]) -> Vec<f32> {
   let min: f32 = 0.018;
   let mul: f32 = 4.5;
   
+  let maxvals = 72100; // 2^16 * 1.099 plus some margin
+  let mut powlookup: Vec<f32> = vec![0.0; maxvals+1];
+  for i in 0..(maxvals+1) {
+    let v = (i as f32) / (maxvals as f32);
+    powlookup[i] = v.powf(g);
+  }
 
   let mut pos = 0;
   for val in inb {
     out[pos] = if *val <= min {
       mul * val
     } else {
-      ((1.0+f) * val).powf(g) - f
+      let part = (1.0+f) * val;
+      let power = powlookup[(part.max(0.0)*(maxvals as f32)).min(maxvals as f32) as usize];
+      power - f
     };
 
     pos += 1;
