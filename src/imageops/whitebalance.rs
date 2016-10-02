4ba0a7a992f6d6c9384f68cf25f81c6ee71621b7
Pedro Côrte-Real <pedro@pedrocr.net>
2016-10-02 01:01:47 +0100

    whitebalance: cleanup loops with proper iterators

--- src/imageops/whitebalance.rs
+++ src/imageops/whitebalance.rs
@@ -6,11 +6,10 @@ pub fn whitebalance(img: &Image, buf: &mut Vec<f32>) {
   let unity: f32 = img.wb_coeffs[1];
   let mul = img.wb_coeffs.iter().map(|x| if x.is_nan() { 1.0 } else { x / unity }).collect::<Vec<f32>>();
 
-  for row in 0..img.height {
-    for col in 0..img.width {
-      let pos = row*img.width + col;
+  for (row,line) in buf.chunks_mut(img.width).enumerate() {
+    for (col,pix) in line.chunks_mut(1).enumerate() {
       let color = fcol(img, row, col);
-      buf[pos] = (buf[pos] * mul[color]).min(1.0);
+      pix[0] = (pix[0] * mul[color]).min(1.0);
     }
   }
 }
