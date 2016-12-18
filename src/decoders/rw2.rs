99ed7b10658624ecce2194c24fab238fda39e7a5
Pedro Côrte-Real <pedro@pedrocr.net>
2016-12-18 01:50:32 +0000

    RW2: implement compressed format

--- src/decoders/rw2.rs
+++ src/decoders/rw2.rs
@@ -35,7 +35,7 @@ impl<'a> Decoder for Rw2Decoder<'a> {
     } else if src.len() >= width*height*3/2 {
       decode_12le_wcontrol(src, width, height)
     } else {
-      return Err("Don't know how to decode compressed".to_string());
+      Rw2Decoder::decode_panasonic(src, width, height)
     };
 
     ok_image(camera, width as u32, height as u32, try!(self.get_wb()), image)
@@ -58,5 +58,76 @@ impl<'a> Rw2Decoder<'a> {
     } else {
       Err("Couldn't find WB".to_string())
     }
+  }
+
+  fn decode_panasonic(buf: &[u8], width: usize, height: usize) -> Vec<u16> {
+    let mut pump = BitPumpPanasonic::new(buf);
+    let mut out: Vec<u16> = vec![0; (width*height) as usize];
+
+    let mut sh: i32 = 0;
+    let mut pred: [i32;2] = [0,0];
+    let mut nonz: [i32;2] = [0,0];
+    for row in 0..height {
+      for col in 0..width {
+        let i = col % 14;
+        if i == 0 {
+          pred = [0,0];
+          nonz = [0,0];
+        }
+        if (i % 3) == 2 {
+          sh = 4 >> (3 - pump.get_bits(2));
+        }
+        if nonz[i&1] != 0 {
+          let j = pump.get_bits(8) as i32;
+          if j != 0 {
+            pred[i & 1] -= 0x80 << sh;
+            if pred[i & 1] < 0 || sh == 4 {
+              pred[i & 1] &= !(-1 << sh);
+            }
+            pred[i & 1] += j << sh;
+          }
+        } else {
+          nonz[i & 1] = pump.get_bits(8) as i32;
+          if nonz[i & 1] != 0 || i > 11 {
+            pred[i & 1] = nonz[i & 1] << 4 | (pump.get_bits(4) as i32);
+          }
+        }
+        out[row*width+col] = pred[col & 1] as u16;
+      }
+    }
+
+    out
+  }
+}
+
+pub struct BitPumpPanasonic<'a> {
+  buffer: &'a [u8],
+  pos: usize,
+  nbits: u32,
+}
+
+impl<'a> BitPumpPanasonic<'a> {
+  pub fn new(src: &'a [u8]) -> BitPumpPanasonic {
+    BitPumpPanasonic {
+      buffer: src,
+      pos: 0,
+      nbits: 0,
+    }
+  }
+}
+
+impl<'a> BitPump for BitPumpPanasonic<'a> {
+  fn peek_bits(&mut self, num: u32) -> u32 {
+    if num > self.nbits {
+      self.nbits += 0x4000 * 8;
+      self.pos += 0x4000;
+    }
+    let byte = (self.nbits-num) >> 3 ^ 0x3ff0;
+    let bits = LEu16(self.buffer, byte as usize + self.pos - 0x4000) as u32;
+    (bits >> ((self.nbits-num) & 7)) & (0x0ffffffffu32 >> (32-num))
+  }
+
+  fn consume_bits(&mut self, num: u32) {
+    self.nbits -= num;
   }
 }
