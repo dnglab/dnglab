65c1b8889a7672b9c36055c3aa551932b66aab9c
Pedro Côrte-Real <pedro@pedrocr.net>
2016-12-18 22:35:23 +0000

    demosaic: implement crops

--- src/imageops/demosaic.rs
+++ src/imageops/demosaic.rs
@@ -4,25 +4,31 @@ use imageops::OpBuffer;
 use std::cmp;
 
 pub fn demosaic_and_scale(img: &Image, minwidth: usize, minheight: usize) -> OpBuffer {
+  // Calculate the resulting width/height and top-left corner after crops
+  let width = (img.width as i64 - img.crops[1] - img.crops[3]) as usize;
+  let height = (img.height as i64 - img.crops[0] - img.crops[2]) as usize;
+  let x = img.crops[3] as usize;
+  let y = img.crops[0] as usize;
+
   let scale = if minwidth == 0 || minheight == 0 {
     0
   } else {
-    cmp::min(img.width / minwidth, img.height / minheight)
+    cmp::min(width / minwidth, height / minheight)
   };
 
   if scale <= 1 {
-    full(img)
+    full(img, x, y, width, height)
   } else {
-    scaled(img, cmp::min(scale, 16)) // Never go less than 1/16th demosaic
+    scaled(img, cmp::min(scale, 16), x, y, width, height) // Never go less than 1/16th demosaic
   }
 }
 
-pub fn full(img: &Image) -> OpBuffer {
-  let mut out = OpBuffer::new(img.width, img.height, 4);
+pub fn full(img: &Image, xs: usize, ys: usize, width: usize, height: usize) -> OpBuffer {
+  let mut out = OpBuffer::new(width, height, 4);
 
   // First we set the colors we already have
   out.mutate_lines(&(|line: &mut [f32], row| {
-    for (col, (pixin, pixout)) in img.data[img.width*row..].chunks(1).zip(line.chunks_mut(4)).enumerate() {
+    for (col, (pixout, pixin)) in line.chunks_mut(4).zip(img.data[img.width*(row+ys)+xs..].chunks(1)).enumerate() {
       let color = fcol(img, row, col);
       pixout[color] = pixin[0] as f32;
     }
@@ -31,16 +37,16 @@ pub fn full(img: &Image) -> OpBuffer {
   // Now we go around the image setting the unset colors to the average of the
   // surrounding pixels
   out.mutate_lines(&(|line: &mut [f32], row| {
-    for col in 0..img.width {
+    for col in 0..width {
       let mut sums: [f32; 4] = [0.0;4];
       let mut counts: [u32; 4] = [0; 4];
       let color = fcol(img, row, col);
 
-      for y in (cmp::max(0,(row as isize)-1) as usize) .. cmp::min(img.height, row+2) {
-        for x in (cmp::max(0,(col as isize)-1) as usize) .. cmp::min(img.width, col+2) {
-          let c = fcol(img, y, x);
+      for y in (cmp::max(0,(row as isize)-1) as usize) .. cmp::min(height, row+2) {
+        for x in (cmp::max(0,(col as isize)-1) as usize) .. cmp::min(width, col+2) {
+          let c = fcol(img, y+ys, x+xs);
           if c != color {
-            sums[c] += img.data[y*img.width+x] as f32;
+            sums[c] += img.data[(y+ys)*img.width+(x+xs)] as f32;
             counts[c] += 1;
           }
         }
@@ -57,20 +63,21 @@ pub fn full(img: &Image) -> OpBuffer {
   out
 }
 
-pub fn scaled(img: &Image, scale: usize) -> OpBuffer {
-  let width = img.width/scale;
-  let mut out = OpBuffer::new(width, img.height/scale, 4);
+pub fn scaled(img: &Image, scale: usize, xs: usize, ys: usize, width: usize, height: usize) -> OpBuffer {
+  let nwidth = width/scale;
+  let nheight = height/scale;
+  let mut out = OpBuffer::new(nwidth, nheight, 4);
 
   // Go around the image averaging every block of pixels
   out.mutate_lines(&(|line: &mut [f32], row| {
-    for col in 0..width {
+    for col in 0..nwidth {
       let mut sums: [f32; 4] = [0.0;4];
       let mut counts: [u32; 4] = [0; 4];
 
       for y in row*scale..(row+1)*scale {
         for x in col*scale..(col+1)*scale {
-          let c = fcol(img, y, x);
-          sums[c] += img.data[y*img.width+x] as f32;
+          let c = fcol(img, y+ys, x+xs);
+          sums[c] += img.data[(y+ys)*img.width+(x+xs)] as f32;
           counts[c] += 1;
         }
       }
