dcfc249eae29ba922db60a7c285b2bba7cadf2ba
Pedro Côrte-Real <pedro@pedrocr.net>
2016-12-18 20:23:12 +0000

    level: use threads to speed up

--- src/imageops/level.rs
+++ src/imageops/level.rs
@@ -10,10 +10,12 @@ pub fn level_and_balance(img: &Image, buf: &mut OpBuffer) {
   let unity: f32 = img.wb_coeffs[1];
   let mul = img.wb_coeffs.iter().map(|x| if x.is_nan() { 1.0 } else { x / unity }).collect::<Vec<f32>>();
 
-  for pix in buf.data.chunks_mut(4) {
-    pix[0] = (((pix[0] - mins[0]) / ranges[0]) * mul[0]).min(1.0);
-    pix[1] = (((pix[1] - mins[1]) / ranges[1]) * mul[1]).min(1.0);
-    pix[2] = (((pix[2] - mins[2]) / ranges[2]) * mul[2]).min(1.0);
-    pix[3] = (((pix[3] - mins[3]) / ranges[3]) * mul[3]).min(1.0);
-  }
+  buf.mutate_lines(&(|line: &mut [f32], _| {
+    for pix in line.chunks_mut(4) {
+      pix[0] = (((pix[0] - mins[0]) / ranges[0]) * mul[0]).min(1.0);
+      pix[1] = (((pix[1] - mins[1]) / ranges[1]) * mul[1]).min(1.0);
+      pix[2] = (((pix[2] - mins[2]) / ranges[2]) * mul[2]).min(1.0);
+      pix[3] = (((pix[3] - mins[3]) / ranges[3]) * mul[3]).min(1.0);
+    }
+  }));
 }
