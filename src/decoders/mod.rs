844018cacd21e2215c623eedb218766cb5097113
Pedro Côrte-Real <pedro@pedrocr.net>
2016-12-27 18:30:34 +0000

    Reformat fetch_tag! and fetch_ifd! macros

--- src/decoders/mod.rs
+++ src/decoders/mod.rs
@@ -5,11 +5,23 @@ use std::error::Error;
 use std::panic;
 
 macro_rules! fetch_tag {
-  ($tiff:expr, $tag:expr) => (try!($tiff.find_entry($tag).ok_or(format!("Couldn't find tag {}",stringify!($tag)).to_string())););
+  ($tiff:expr, $tag:expr) => (
+    try!(
+      $tiff.find_entry($tag).ok_or(
+        format!("Couldn't find tag {}",stringify!($tag)).to_string()
+      )
+    );
+  );
 }
 
 macro_rules! fetch_ifd {
-  ($tiff:expr, $tag:expr) => (try!($tiff.find_first_ifd($tag).ok_or(format!("Couldn't find ifd with tag {}",stringify!($tag)).to_string())););
+  ($tiff:expr, $tag:expr) => (
+    try!(
+      $tiff.find_first_ifd($tag).ok_or(
+        format!("Couldn't find ifd with tag {}",stringify!($tag)).to_string()
+      )
+    );
+  );
 }
 
 extern crate toml;
