3ee0d509196869f93601ddb55bfa0d7aad1a6445
Pedro Côrte-Real <pedro@pedrocr.net>
2017-01-21 11:48:11 +0000

    cfa: add some basic documentation

--- src/decoders/cfa.rs
+++ src/decoders/cfa.rs
@@ -2,6 +2,22 @@ use decoders::tiff::*;
 use std::fmt;
 use std::clone;
 
+/// Representation of the color filter array pattern in raw cameras
+///
+/// # Example
+/// ```
+/// use rawloader::CFA;
+/// let cfa = CFA::new("RGGB");
+/// assert_eq!(cfa.color_at(0,0), 0);
+/// assert_eq!(cfa.color_at(0,1), 1);
+/// assert_eq!(cfa.color_at(1,0), 1);
+/// assert_eq!(cfa.color_at(1,1), 2);
+/// ```
+///
+/// You will almost always get your CFA struct from a RawImage decode, already fully
+/// initialized and ready to be used in processing. The color_at() implementation is
+/// designed to be fast so it can be called inside the inner loop of demosaic or other
+/// color-aware algorithms that work on pre-demosaic data
 pub struct CFA {
   pub name: String,
   pub width: usize,
@@ -24,6 +40,14 @@ impl CFA {
     CFA::new(&patname)
   }
 
+  /// Create a new CFA from a string describing it. For simplicity the pattern is specified
+  /// as each pixel being one of R/G/B/E representing the 0/1/2/3 colors in a 4 color image.
+  /// The pattern is specified as the colors in each row concatenated so RGGB means that
+  /// the first row is RG and the second row GB. Row size is determined by pattern size
+  /// (e.g., the xtrans pattern is 6x6 and thus 36 characters long). In theory this could
+  /// lead to confusion between different pattern sizes but in practice there are only
+  /// a few oddball cameras no one cares about that do anything but 2x2 and 6x6 (and those
+  /// work fine with this as well).
   pub fn new(patname: &str) -> CFA {
     let (width, height) = match patname.len() {
       0 => (0,0),
@@ -65,10 +89,30 @@ impl CFA {
     }
   }
 
+  /// Get the color index at the given position. Designed to be fast so it can be called
+  /// from inner loops without performance issues.
   pub fn color_at(&self, row: usize, col: usize) -> usize {
     self.pattern[(row+48) % 48][(col+48) % 48]
   }
 
+  /// Shift the pattern left and/or down. This is useful when cropping the image to get
+  /// the equivalent pattern of the crop when it's not a multiple of the pattern size.
+  ///
+  /// # Example
+  /// ```
+  /// use rawloader::CFA;
+  /// let cfa = CFA::new("RGGB");
+  /// assert_eq!(cfa.color_at(0,0), 0);
+  /// assert_eq!(cfa.color_at(0,1), 1);
+  /// assert_eq!(cfa.color_at(1,0), 1);
+  /// assert_eq!(cfa.color_at(1,1), 2);
+  ///
+  /// let shifted = cfa.shift(1,1);
+  /// assert_eq!(shifted.color_at(0,0), 2);
+  /// assert_eq!(shifted.color_at(0,1), 1);
+  /// assert_eq!(shifted.color_at(1,0), 1);
+  /// assert_eq!(shifted.color_at(1,1), 0);
+  /// ```
   pub fn shift(&self, x: usize, y: usize) -> CFA {
     let mut pattern: [[usize;48];48] = [[0;48];48];
     for row in 0..48 {
