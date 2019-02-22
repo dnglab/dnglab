fed9307b8a993fb29be44a02778dc3a6bf791880
Pedro Côrte-Real <pedro@pedrocr.net>
2019-02-22 14:28:46 +0000

    Add a basic identify program to check a file

---
+++ src/bin/identify.rs
@@ -0,0 +1,16 @@
+extern crate rawloader;
+
+use std::env;
+
+fn main() {
+  let args: Vec<_> = env::args().collect();
+  if args.len() != 2 {
+    println!("Usage: {} <file>", args[0]);
+    std::process::exit(2);
+  }
+  let file = &args[1];
+  match rawloader::decode_file(file) {
+    Ok(_)  => println!("OK file"),
+    Err(_) => println!("FAILED file"),
+  }
+}
