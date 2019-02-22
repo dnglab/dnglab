2a1dbe4417dfb4ff6ecf54960c6109a6058a8496
Pedro Côrte-Real <pedro@pedrocr.net>
2019-02-22 09:38:11 +0000

    Tighten the conditions for image sizing
    Now that images can't be too large they also shouldn't have any
    dimension that's too big. Otherwise it's easy to cause a hang. afl-fuzz
    found a case of a 12 pixel wide but 17 million pixel tall image that
    would then hang because threaded decode meant it had to panic every
    single line.

--- src/decoders/mod.rs
+++ src/decoders/mod.rs
@@ -28,8 +28,8 @@ macro_rules! fetch_ifd {
 macro_rules! alloc_image {
   ($width:expr, $height:expr) => (
     {
-      if $width * $height > 500000000 {
-        panic!("rawloader: surely there's no such thing as a >500MP image!");
+      if $width * $height > 500000000 || $width > 50000 || $height > 50000 {
+        panic!("rawloader: surely there's no such thing as a >500MP or >50000 px wide/tall image!");
       }
       let mut out: Vec<u16> = vec![0; $width * $height];
       out
