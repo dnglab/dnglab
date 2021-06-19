523cc0816548fb0fb33f597d0889cf5d1d6a4a37
Daniel Vogelbacher <daniel@chaospixel.com>
2021-06-19 15:58:43 +0200

    Devtools: dump_image_u16()

---
+++ rawler/src/devtools/mod.rs
@@ -0,0 +1,6 @@
+use image::{ImageBuffer, ImageFormat, Luma};
+
+pub fn dump_image_u16(data: &Vec<u16>, width: usize, height: usize, path: impl AsRef<str>) {
+  let img = ImageBuffer::<Luma<u16>, Vec<u16>>::from_vec(width as u32, height as u32, data.clone()).unwrap();
+  img.save_with_format(path.as_ref(), ImageFormat::Tiff).unwrap();
+}
