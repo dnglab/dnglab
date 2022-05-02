aaf5014aa7e299171e317b6ddba14155e375a59a
Daniel Vogelbacher <daniel@chaospixel.com>
2022-05-02 18:48:05 +0200

    ERF: Extract blacklevel from makernotes

--- rawler/src/decoders/erf.rs
+++ rawler/src/decoders/erf.rs
@@ -65,7 +65,12 @@ impl<'a> Decoder for ErfDecoder<'a> {
 
     let image = decode_12be_wcontrol(&src, width, height, dummy);
     let cpp = 1;
-    ok_image(self.camera.clone(), width, height, cpp, self.get_wb()?, image.into_inner())
+
+    let mut img = RawImage::new(self.camera.clone(), width, height, cpp, self.get_wb()?, image.into_inner(), dummy);
+    if let Some(blacklevel) = self.get_blacklevel() {
+      img.blacklevels = blacklevel;
+    }
+    Ok(img)
   }
 
   fn format_dump(&self) -> FormatDump {
@@ -89,5 +94,12 @@ impl<'a> ErfDecoder<'a> {
       let b = BEu16(levels.get_data(), 50) as f32;
       Ok([r * 508.0 * 1.078 / 65536.0, 1.0, b * 382.0 * 1.173 / 65536.0, NAN])
     }
+  }
+
+  fn get_blacklevel(&self) -> Option<[u16; 4]> {
+    if let Some(levels) = self.makernote.get_entry(0x0401) {
+      return Some([levels.force_u16(0), levels.force_u16(1), levels.force_u16(2), levels.force_u16(3)]);
+    }
+    return None;
   }
 }
