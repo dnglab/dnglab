f4ed47004ff4622a6bfc229cfa022e17770c04d9
William-niq <william.vanbeek@nielseniq.com>
2025-09-22 12:21:47 +0200

    Fix build warnings

--- rawler/src/pixarray.rs
+++ rawler/src/pixarray.rs
@@ -1,44 +1,11 @@
 use std::cell::UnsafeCell;
 
-use multiversion::multiversion;
 use rayon::prelude::*;
 
-use crate::{
-  decoders::decode_threaded_prealloc,
-  formats::tiff::Rational,
-  imgop::{Dim2, Point, Rect},
-};
-
-pub trait SubPixel: Default + std::fmt::Debug + Clone + Copy + Send + Sync + Into<Rational> {
-  fn as_f32(self) -> f32;
-  fn as_u16(self) -> u16;
-}
-
-impl SubPixel for u16 {
-  fn as_f32(self) -> f32 {
-    self as f32
-  }
-
-  fn as_u16(self) -> u16 {
-    self as u16
-  }
-}
-impl SubPixel for f32 {
-  fn as_f32(self) -> f32 {
-    self as f32
-  }
-
-  fn as_u16(self) -> u16 {
-    self as u16
-  }
-}
-
-pub type LineMut<'a, T> = &'a mut [T];
-
-pub type Line<'a, T> = &'a [T];
+use crate::imgop::{Dim2, Point, Rect};
 
 #[derive(Clone)]
-pub struct Pix2D<T: SubPixel> {
+pub struct Pix2D<T> {
   pub width: usize,
   pub height: usize,
   pub data: Vec<T>,
@@ -50,7 +17,7 @@ pub type PixF32 = Pix2D<f32>;
 
 impl<T> Pix2D<T>
 where
-  T: SubPixel,
+  T: Copy + Default + Send,
 {
   pub fn new_with(data: Vec<T>, width: usize, height: usize) -> Self {
     assert_eq!(data.len(), height * width);
@@ -88,10 +55,6 @@ where
 
   pub fn into_inner(self) -> Vec<T> {
     self.data
-  }
-
-  pub fn len(&self) -> usize {
-    self.data.len()
   }
 
   pub fn dim(&self) -> Dim2 {
@@ -137,20 +100,6 @@ where
   }
 
   #[inline(always)]
-  pub fn row(&self, row: usize) -> &[T] {
-    debug_assert!(self.initialized);
-    let start = row * self.width;
-    &self.data[start..start + self.width]
-  }
-
-  #[inline(always)]
-  pub fn row_mut(&mut self, row: usize) -> &mut [T] {
-    debug_assert!(self.initialized);
-    let start = row * self.width;
-    &mut self.data[start..start + self.width]
-  }
-
-  #[inline(always)]
   pub fn at(&self, row: usize, col: usize) -> &T {
     debug_assert!(self.initialized);
     #[cfg(debug_assertions)]
@@ -181,7 +130,7 @@ where
   where
     F: Fn(T) -> T + Send + Sync,
   {
-    assert!(self.initialized);
+    debug_assert!(self.initialized);
     self.data.par_iter_mut().for_each(|v| *v = op(*v));
   }
 
@@ -190,7 +139,7 @@ where
   where
     F: Fn(T, usize, usize) -> T,
   {
-    assert!(self.initialized);
+    debug_assert!(self.initialized);
     self
       .pixel_rows_mut()
       .enumerate()
@@ -198,82 +147,18 @@ where
   }
 
   pub fn crop(&self, area: Rect) -> Self {
-    assert!(self.initialized);
-    crop(&self, area)
-  }
-
-  pub fn into_crop(self, area: Rect) -> Self {
-    if self.dim() == area.d && area.p == Point::zero() {
-      self // No-Op
-    } else {
-      crop(&self, area)
-    }
-  }
-}
-
-#[multiversion(targets("x86_64+avx+avx2", "x86+sse", "aarch64+neon"))]
-fn crop<T>(pixbuf: &Pix2D<T>, area: Rect) -> Pix2D<T>
-where
-  T: SubPixel,
-{
-  let mut output;
-  if pixbuf.initialized {
-    output = Pix2D::<T>::new(area.width(), area.height());
-
-    output.par_pixel_rows_mut().enumerate().for_each(|(row, line)| {
-      let src_row = pixbuf.row(area.y() + row);
-      line.copy_from_slice(&src_row[area.x()..area.x() + line.len()]);
-    });
-  } else {
-    output = Pix2D::<T>::new_uninit(area.width(), area.height());
-  }
-  output
-}
-
-#[multiversion(targets("x86_64+avx+avx2", "x86+sse", "aarch64+neon"))]
-pub(crate) fn deinterleave2x2<T>(pixbuf: &Pix2D<T>) -> crate::Result<Pix2D<T>>
-where
-  T: SubPixel,
-{
-  if pixbuf.initialized {
-    let mut output = Pix2D::<T>::new(pixbuf.width, pixbuf.height);
-
-    let line_width = pixbuf.width;
-
-    let half_width = line_width / 2;
-    let line_distance = line_width;
-    let ch0 = &pixbuf[..];
-    let ch1 = &pixbuf[half_width..];
-    let ch2 = &pixbuf[pixbuf.len() / 2..];
-    let ch3 = &pixbuf[pixbuf.len() / 2 + half_width..];
-
-    decode_threaded_prealloc(&mut output, &|line, row| {
-      let src_row = row / 2;
-      let offset = src_row * line_distance;
-      let ch_a;
-      let ch_b;
-      if row & 1 == 0 {
-        // For even rows, we take top-left and top-right channel data.
-        ch_a = &ch0[offset..offset + half_width];
-        ch_b = &ch1[offset..offset + half_width];
-      } else {
-        // For odd rows, we take bottom-left and bottom-right channel data.
-        ch_a = &ch2[offset..offset + half_width];
-        ch_b = &ch3[offset..offset + half_width];
-      }
-
-      debug_assert_eq!(ch_a.len(), ch_b.len());
-      debug_assert_eq!(ch_a.len() + ch_b.len(), line.len());
-      debug_assert_eq!(line_width, line.len());
-      line.chunks_exact_mut(2).zip(ch_a.iter().zip(ch_b.iter())).for_each(|(dst, (a, b))| {
-        dst[0] = *a;
-        dst[1] = *b;
-      });
-      Ok(())
-    })?;
-    Ok(output)
-  } else {
-    Ok(pixbuf.clone())
+    debug_assert!(self.initialized);
+    let mut output = Vec::with_capacity(area.d.h * area.d.w);
+    output.extend(
+      self
+        .pixels()
+        .chunks_exact(self.width)
+        .skip(area.p.y)
+        .take(area.d.h)
+        .flat_map(|row| row[area.p.x..area.p.x + area.d.w].iter())
+        .cloned(),
+    );
+    Self::new_with(output, area.d.w, area.d.h)
   }
 }
 
@@ -289,7 +174,6 @@ impl<T> Index<usize> for Pix2D<T> {
 impl<I, T> std::ops::Index<I> for Pix2D<T>
 where
   I: std::slice::SliceIndex<[T]>,
-  T: SubPixel,
 {
   type Output = I::Output;
 
@@ -301,7 +185,6 @@ where
 impl<I, T> std::ops::IndexMut<I> for Pix2D<T>
 where
   I: std::slice::SliceIndex<[T]>,
-  T: SubPixel,
 {
   fn index_mut<'a>(&mut self, index: I) -> &mut Self::Output {
     &mut self.data[index]
@@ -325,14 +208,11 @@ where
  */
 
 /// An ugly hack to get multiple mutable references to Pix2D
-pub struct SharedPix2D<T: SubPixel> {
+pub struct SharedPix2D<T> {
   pub inner: UnsafeCell<Pix2D<T>>,
 }
 
-impl<T> SharedPix2D<T>
-where
-  T: SubPixel,
-{
+impl<T> SharedPix2D<T> {
   pub fn new(inner: Pix2D<T>) -> Self {
     Self { inner: inner.into() }
   }
@@ -351,7 +231,7 @@ where
   }
 }
 
-unsafe impl<T> Sync for SharedPix2D<T> where T: SubPixel {}
+unsafe impl<T> Sync for SharedPix2D<T> where T: Copy + Default + Send {}
 
 #[derive(Clone)]
 pub struct Color2D<T, const N: usize> {
@@ -481,6 +361,19 @@ where
     );
     Self::new_with(output, area.d.w, area.d.h)
   }
+
+  pub fn make_padded(&self, padding: usize) -> Self {
+    let new_w = self.width + padding * 2;
+    let new_h = self.height + padding * 2;
+    let mut padded = Self::new(new_w, new_h);
+
+    for y in 0..self.height {
+      for x in 0..self.width {
+        *padded.at_mut(y + padding, x + padding) = *self.at(y, x);
+      }
+    }
+    padded
+  }
 }
 
 impl<T, const N: usize> Default for Color2D<T, N>
@@ -527,20 +420,6 @@ where
 }
 
 unsafe impl<T, const N: usize> Sync for Color2DPtr<T, N> {}
-
-#[macro_export]
-macro_rules! alloc_image_f32_plain {
-  ($width:expr, $height:expr, $dummy: expr) => {{
-    if $width * $height > 500000000 || $width > 50000 || $height > 50000 {
-      panic!("rawler: surely there's no such thing as a >500MP or >50000 px wide/tall image!");
-    }
-    if $dummy {
-      $crate::pixarray::PixF32::new_uninit($width, $height)
-    } else {
-      $crate::pixarray::PixF32::new($width, $height)
-    }
-  }};
-}
 
 #[macro_export]
 macro_rules! alloc_image_plain {
