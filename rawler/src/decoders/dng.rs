3e07a9ee4d30c1c9696ad2989f2bb7421dbe85c4
phence <phence@users.noreply.github.com>
2026-01-01 17:59:37 +0100

    Use DngTag enum for white balance

--- rawler/src/decoders/dng.rs
+++ rawler/src/decoders/dng.rs
@@ -291,9 +291,9 @@ impl<'a> DngDecoder<'a> {
   }
 
   fn get_wb(&self, cam: &Camera) -> Result<[f32; 4]> {
-    if let Some(levels) = self.tiff.get_entry(TiffCommonTag::AsShotNeutral) {
+    if let Some(levels) = self.tiff.get_entry(DngTag::AsShotNeutral) {
       Ok([1.0 / levels.force_f32(0), 1.0 / levels.force_f32(1), 1.0 / levels.force_f32(2), f32::NAN])
-    } else if let Some(levels) = self.tiff.get_entry(TiffCommonTag::AsShotWhiteXY) {
+    } else if let Some(levels) = self.tiff.get_entry(DngTag::AsShotWhiteXY) {
       let mut wb_coeffs = [f32::NAN, f32::NAN, f32::NAN, f32::NAN];
       let x = levels.force_f32(0);
       let y = levels.force_f32(1);
